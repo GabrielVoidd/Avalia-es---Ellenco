12f685372c3c7f3bbd1a64adc64ab70bc41fe86c
Gabriel <souzagabriel0006@gmail.com>
2026-03-09 15:10:21 -0300

    Adicionando format ao forms para a data ser mantida ao editar o registro

--- avaliacao/forms.py
+++ avaliacao/forms.py
@@ -10,8 +10,8 @@ class AvaliacaoForm(forms.ModelForm):
             'nome_estagiario': forms.TextInput(attrs={'class': 'form-control'}),
             'empresa': forms.TextInput(attrs={'class': 'form-control'}),
             'instituicao_ensino': forms.TextInput(attrs={'class': 'form-control'}),
-            'data_inicio': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'data_fim': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'data_inicio': forms.DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'}),
+            'data_fim': forms.DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'}),
             'status': forms.Select(attrs={'class': 'form-select'}),
             'observacoes': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
         }
